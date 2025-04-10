98d53b89e63f0e2dab5455c9fb522741df28f480
Morgan Kaponga <116386982+97kiwi101@users.noreply.github.com>
2025-04-09 19:35:27 -0500

    changed

--- flaskapp.py
+++ flaskapp.py
@@ -1,21 +1,33 @@
-from flask import Flask, render_template
-import mysql.connector
-from creds import db_config
+# author: T. Urness and M. Moore
+# description: Flask example using redirect, url_for, and flash
+# credit: the template html files were constructed with the help of ChatGPT
+
+from flask import Flask
+from flask import render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
+from matplotlib import table
+from botocore.exceptions import ClientError
+import boto3
+
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+table = dynamodb.Table('UserAccountsPoems') 
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key' # this is an artifact for using flash displays; 
+                                   # it is required, but you can leave this alone
 
 @app.route('/')
-def index():
-    connection = mysql.connector.connect(**db_config)
-    cursor = connection.cursor(dictionary=True)
-    
-    cursor.execute("SELECT * FROM ProjectOnePoems")
-    rows = cursor.fetchall()
-    
-    cursor.close()
-    connection.close()
-    
-    return render_template('index.html', rows=rows)
+def home():
+    try:
+        response = table.scan()
+        users_list = response.get('Items', [])
+    except ClientError as e:
+        flash(f"Error reading from DynamoDB: {e.response['Error']['Message']}", 'danger')
+        users_list = []
 
+    return render_template('display_users.html', users=users_list)
+
+# these two lines of code should always be the last in the file
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
+    
